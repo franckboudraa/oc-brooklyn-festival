9ba6dc71808b6e11c9786d4fce4485ba3be4d263
neocydfr <franckboudraa@gmail.com>
2018-01-10 18:43:51 +0100

    added textarea to accessbility setting

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
   if (document.cookie === 'handicap=on') {
-    $('body,h1,h2,h3,h4,h5,h6,button,input').css("font-family", 'Arial, sans-serif');
+    $('body,h1,h2,h3,h4,h5,h6,button,input,textarea').css("font-family", 'Arial, sans-serif');
     $('#handicap').html('<i class="fa fa-toggle-on"></i> <i class="fa fa-wheelchair"></i>');
   }
   $('#handicap').on('click', function (e) {
     if (document.cookie === 'handicap=on') {
       $('h1,h2,h3,h4,h5,h6').css("font-family", 'Cabin Sketch, cursive');
-      $('body,button,input').css("font-family", '"Neucha", -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif');
+      $('body,button,input,textarea').css("font-family", '"Neucha", -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif');
       $('#handicap').html('<i class="fa fa-toggle-off"></i> <i class="fa fa-wheelchair"></i>');
       document.cookie = 'handicap=off';
     } else {
-      $('body,h1,h2,h3,h4,h5,h6,button,input').css("font-family", 'Arial, sans-serif');
+      $('body,h1,h2,h3,h4,h5,h6,button,input,textarea').css("font-family", 'Arial, sans-serif');
       $('#handicap').html('<i class="fa fa-toggle-on"></i> <i class="fa fa-wheelchair"></i>');
       document.cookie = 'handicap=on';
     }
