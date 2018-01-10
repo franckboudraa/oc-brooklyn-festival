f7720d189881f4bc6c8567f50b7dc851ecc440cc
neocydfr <franckboudraa@gmail.com>
2018-01-10 18:35:21 +0100

    working contact form and improved bg responsivity

--- js/main.js
+++ js/main.js
@@ -15,10 +15,21 @@ $(document).ready(function () {
       document.cookie = 'handicap=on';
     }
   });
-  $('#regform').on('submit', function (e) {
+  $('form#reg').on('submit', function (e) {
     e.preventDefault();
     $('div#prereg').replaceWith('<div class="offset-2 col-8 alert alert-success" role="alert">\n' +
       '  <h4><i class="fa fa-check-circle"></i> You successfully pre-registered! Thank you!</h4>\n' +
+      '</div>');
+  });
+  $('form#contact').on('submit', function (e) {
+    e.preventDefault();
+    $('div#contactform').replaceWith('' +
+      '<div class="col-8 offset-2" id="contactform"><h1>Contact us</h1>\n' +
+      ' <p><i class="fa fa-question-circle"></i> Want to know how to access to the festival ?' +
+      '   <a href="access.html">Check the Practical info page</a></p>' +
+      ' <div class="col-12 alert alert-success" role="alert">\n' +
+      '  <h4><i class="fa fa-check-circle"></i> Your email has been sent! Thank you!</h4>\n' +
+      ' </div>' +
       '</div>');
   });
 });
